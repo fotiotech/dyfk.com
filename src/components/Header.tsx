5becaa8c2258f3abd9cb68b0404ff1f07a7a059c
dylan/ <dilanefotio14@gmail.com>
2024-08-23 16:23:11 +0100

    updated hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -110,7 +110,7 @@ const Header = () => {
         </div>
       </div>
       <div>
-        <ul className="whitespace-nowrap overflow-auto">
+        <ul className="whitespace-nowrap overflow-auto scrollbar-none">
           {category &&
             category.map((cat, index) => (
               <li key={index} className="inline-block pt-2 px-2">
