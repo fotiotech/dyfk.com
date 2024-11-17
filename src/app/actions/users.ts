25e79d651b2769f77e0d355db93fe5c3fe8cdbfa
dylan/ <dilanefotio14@gmail.com>
2024-11-17 20:19:47 +0100

    update gigs

--- src/app/actions/users.ts
+++ src/app/actions/users.ts
@@ -5,6 +5,14 @@ import { connection } from "@/utils/connection";
 export async function findUsers(_id?: string) {
   await connection();
   if (_id) {
+    const data = await User.findOne({ _id });
+    return {
+      ...data?.toObject(),
+      _id: data._id?.toString(),
+      created_at: data?.created_at.toISOString(),
+      updated_at: data?.updated_at.toISOString(),
+    };
+  } else {
     const data = await User.find();
     return data.map((res) => ({
       ...res.toObject(),
