577264a0b2992091399c00f6d09c210f8f542638
dylan/ <dilanefotio14@gmail.com>
2024-12-01 22:37:16 +0100

    update gigs

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,7 +2,44 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Suspense } from "react";
 import Loading from "../loading";
-import Head from "next/head";
+
+export const metadata = {
+  title: "dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun",
+  description:
+    "Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience.",
+  viewport: "width=device-width, initial-scale=1",
+  canonical: "https://dyfk-com.vercel.app",
+
+  // Open Graph Metadata
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://dyfk-com.vercel.app",
+    siteName: "dyfkCameroun.com",
+    title: "dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun",
+    description:
+      "Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience.",
+    images: [
+      {
+        url: "https://dyfk-com.vercel.app/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun",
+      },
+    ],
+  },
+
+  // Twitter Metadata
+  twitter: {
+    card: "summary_large_image",
+    site: "@dyfkCameroun",
+    creator: "@dyfkCameroun",
+    title: "dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun",
+    description:
+      "Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience.",
+    image: "https://dyfk-com.vercel.app/logo.png",
+  },
+};
 
 export default function Layout({
   children,
@@ -10,59 +47,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex flex-col ">
-      <Head>
-        <title>
-          dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun
-        </title>
-        <meta
-          name="description"
-          content="Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://dyfk-com.vercel.app" />
-
-        {/* Open Graph Metadata */}
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:url" content="https://dyfk-com.vercel.app" />
-        <meta property="og:site_name" content="dyfkCameroun.com" />
-        <meta
-          property="og:title"
-          content="dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun"
-        />
-        <meta
-          property="og:description"
-          content="Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience."
-        />
-        <meta
-          property="og:image"
-          content="https://dyfk-com.vercel.app/logo.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta
-          property="og:image:alt"
-          content="dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun"
-        />
-
-        {/* Twitter Metadata */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dyfkCameroun" />
-        <meta name="twitter:creator" content="@dyfkCameroun" />
-        <meta
-          name="twitter:title"
-          content="dyfkCameroun.com - Your Trusted E-Commerce Platform in Cameroun"
-        />
-        <meta
-          name="twitter:description"
-          content="Discover the best products at unbeatable prices on dyfkCameroun.com. Shop now for a seamless online shopping experience."
-        />
-        <meta
-          name="twitter:image"
-          content="https://dyfk-com.vercel.app/logo.png"
-        />
-      </Head>
+    <div className="flex flex-col">
       <div className="flex flex-col min-h-screen">
         <Header />
         <Suspense fallback={<Loading />}>
